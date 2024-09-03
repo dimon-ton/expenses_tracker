84499493e16284ac6a8e769e6eb9bc6a0bbd9d7e
dimon-ton <saichon2namoom@gmail.com>
2024-09-03 07:05:45 +0700

    reach at section 103

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -28,9 +29,9 @@ class _ExpensesState extends State<Expenses> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: const [
-        Text('The chart'),
-        Text('Expenses list...'),
+      children: [
+        const Text('The chart'),
+        Expanded(child: ExpensesList(expenses: _registeredExpenses)),
       ],
     ));
   }
