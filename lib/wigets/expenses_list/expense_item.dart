3104733ef9f6fa7457817cd64a9be12bf121ae14
dimon-ton <saichon2namoom@gmail.com>
2024-09-11 06:48:59 +0700

    section 112

--- lib/wigets/expenses_list/expense_item.dart
+++ lib/wigets/expenses_list/expense_item.dart
@@ -21,7 +21,7 @@ class ExpenseItem extends StatelessWidget {
             ),
             Row(
               children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Text('\฿ ${expense.amount.toStringAsFixed(2)}'),
                 const Spacer(),
                 Row(
                   children: [
--- lib/wigets/new_expense.dart
+++ lib/wigets/new_expense.dart
@@ -10,10 +10,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -30,11 +32,24 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\฿ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'),
               ),
