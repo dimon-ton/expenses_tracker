8de99929de0099299f803e4bef68c43236b63024
dimon-ton <saichon2namoom@gmail.com>
2024-09-16 06:26:23 +0700

    section 124 Getting Started with Theming

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:expense_tracker/wigets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: false),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color.fromARGB(255, 135, 46, 151)),
       home: const Expenses(),
     ),
   );
