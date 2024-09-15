80c2354d68c15d1920d1e84941b7a74ccaeb6bb7
dimon-ton <saichon2namoom@gmail.com>
2024-09-15 08:53:04 +0700

    section 122 Showing & Managing 'Snackbars'

--- lib/wigets/expenses.dart
+++ lib/wigets/expenses.dart
@@ -42,13 +42,40 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Expense Tracker'),
@@ -62,11 +89,7 @@ class _ExpensesState extends State<Expenses> {
         body: Column(
           children: [
             const Text('The chart'),
-            Expanded(
-                child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            )),
+            Expanded(child: mainContent),
           ],
         ));
   }
