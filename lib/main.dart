db13a799e12f4493ff79aa6602f660f6b1696ecd
dimon-ton <saichon2namoom@gmail.com>
2024-09-17 06:23:37 +0700

    section 126 Setting Text Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/wigets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 231, 66, 116));
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          scaffoldBackgroundColor: const Color.fromARGB(255, 135, 46, 151)),
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
